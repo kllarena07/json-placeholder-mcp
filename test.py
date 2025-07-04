881e0a51ab9341f7fb85fe4fff378f5eb81933cc
Kieran Llarena <baymax709@gmail.com>
2025-07-04 18:39:35 -0400

    chore: comment output structure

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ import requests
 
 res = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 print(res.json())
+
+# output:
+# {
+#   'userId': 1,
+#   'id': 1,
+#   'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+#   'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+# }
