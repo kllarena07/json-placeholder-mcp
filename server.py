637e9f953328728cae740d312d91a768848f9cad
Kieran Llarena <baymax709@gmail.com>
2025-07-04 18:56:44 -0400

    feat: get one post from api

--- server.py
+++ server.py
@@ -10,5 +10,12 @@ def get_all_posts(
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     return response.json()
 
+@mcp.tool()
+def get_one_post(
+    description="Get one post from JSONPlaceholder"
+):
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    return response.json()
+
 if __name__ ==  "__main__":
     mcp.run()
